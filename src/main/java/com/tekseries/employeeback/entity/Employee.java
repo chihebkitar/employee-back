1918e5e186b6dd108fdf9e017589689ad5b15e2c
chihebkitar <chihebkitar11@gmail.com>
2024-09-05 04:04:45 +0200

    cicd

---
+++ src/main/java/com/tekseries/employeeback/entity/Employee.java
@@ -0,0 +1,28 @@
+package com.tekseries.employeeback.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "First_name",nullable = false)
+    private String firstname;
+    @Column(name = "Last_name",nullable = false)
+    private String lastname;
+    private String email;
+
+    public Employee(String firstname, String lastname, String email) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+    }
+}
